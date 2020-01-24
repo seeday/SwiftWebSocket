2d900aa9c7c9b182557f6412dfc161fa64517df2
Chris Day <christopher_day@eogresources.com>
2020-01-24 09:15:47 -0600

    Update SPM

--- Package.swift
+++ Package.swift
@@ -1,5 +1,17 @@
+// swift-tools-version:5.1
+
 import PackageDescription
 
 let package = Package(
-    name: "SwiftWebSocket"
+    name: "SwiftWebSocket",
+    products: [
+.library(name: "SwiftWebSocket", targets:["SwiftWebSocket"])
+],
+targets: [
+.target(name:"SwiftWebSocket",
+path: "Source"),
+.testTarget(name: "SwiftWebSocketTests",
+dependencies: ["SwiftWebSocket"],
+path: "Test")
+]
 )
